5a9cd09799756dc10c33f75a963f68db9600a643
Abeloider <abeljiangwan@gmail.com>
2025-09-20 23:37:44 +0200

    fibonnacci algo avanzado

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,39 +5,34 @@ int main() {
     int t;
     cin >> t; // n�mero de consultas
 
-    const int periodo = 300;
-    int fib[periodo + 1];
-    long long pref[periodo + 1];
+    int x = 300; 
+
+    int fib[x + 1];
 
     // primeros dos valores
-    fib[1] = 1;
-    fib[2] = 1;
+    fib[1] = 1; // cogemos la primera posicion e lo igualamos a 1
+    fib[2] = 1; // cogemos la segunda posicion e lo igualamos a 1
 
-    // generamos la secuencia especial mod 100
-    for (int i = 3; i <= periodo; i++) {
-        fib[i] = (fib[i-1] + fib[i-2]) % 100;
+    // generamos fibonnacci
+    for (int i = 3; i <= x; i++) {
+        fib[i] = (fib[i-1] + fib[i-2])%100; // llegamos hasta la posicion 100
     }
 
-    // suma acumulada
-    pref[0] = 0;
-    for (int i = 1; i <= periodo; i++) {
-        pref[i] = pref[i-1] + fib[i];
-    }
-    long long totalPeriodo = pref[periodo];
-
+    int pos1;
+    int pos2;
+    double sumatotal;
+    
     while (t--) {
-        long long a, b;
-        cin >> a >> b;
-
-        auto sumaHasta = [&](long long x) -> long long {
-            if (x <= 0) return 0;
-            long long ciclos = x / periodo;
-            int resto = x % periodo;
-            return ciclos * totalPeriodo + pref[resto];
-        };
-
-        long long resultado = sumaHasta(b) - sumaHasta(a-1);
-        cout << resultado << "\n";
+        cin >> pos1 >> pos2 ;
+        sumatotal = 0;
+        if (pos1 > 100 || pos2 > 100) {
+            pos1 = pos1 % 100;
+            pos2 = pos2 % 100;  
+        }
+        for (int i = pos1; i <= pos2; i++) {
+            sumatotal += fib[i];  
+    }
+    cout << sumatotal << endl; 
     }
     return 0;
 }
